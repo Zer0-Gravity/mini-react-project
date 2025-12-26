1ca4d75ba9bcf14d3d7f2fc610b0986e1c733f79
Zer0-Gravity <muslikhulimam878@gmail.com>
2025-12-26 14:22:46 +0700

    Add: Conditional rendering and fetch currencies name

--- currency-exchange-app/src/Component/CurrencyApp.tsx
+++ currency-exchange-app/src/Component/CurrencyApp.tsx
@@ -13,37 +13,57 @@ interface Rates {
     [key: string]: string;
 }
 
+interface Currency {
+    [key: string]: number;
+}
+
 function CurrencyApp() {
     const [currFrom, setCurrFrom] = useState<string>("");
     const [currTo, setCurrTo] = useState<string>("");
     const [data, setData] = useState<Data | null>(null);
+    const [currency, setCurrency] = useState<Currency>();
 
     useEffect(() => {
         const fetchData = async () => {
-            if (currFrom && currTo) {
-                await axios
-                    .get(
-                        `https://api.frankfurter.dev/v1/latest?base=${currFrom}&symbols=${currTo}`
-                    )
-                    .then((response) => {
-                        setData(response.data);
-                    });
+            try {
+                if (currFrom && currTo) {
+                    await axios
+                        .get(
+                            `https://api.frankfurter.dev/v1/latest?base=${currFrom}&symbols=${currTo}`
+                        )
+                        .then((response) => {
+                            setData(response.data);
+                        });
+
+                    await axios
+                        .get("https://api.frankfurter.dev/v1/currencies")
+                        .then((response) => {
+                            setCurrency(response.data);
+                        });
+                }
+            } catch (error) {
+                console.log("Error fetching", error);
             }
         };
 
         fetchData();
     }, [currFrom, currTo]);
 
-    console.log(data);
+    console.log(data?.rates);
 
     return (
         <div className="flex w-225 p-2 bg-amber-50 ">
             <div className="flex flex-col gap-3">
                 <h1 className="text-2xl font-bold">CURRENCY CONVERTER</h1>
-                <div>
-                    <p>1 money equals to</p>
-                    <h1 className="text-4xl">0,0000 This Big Money</h1>
-                </div>
+                {data && currency ? (
+                    <div>
+                        <p>{`${data.amount}  ${currency[currFrom]} is equal to`}</p>
+                        <h1 className="text-4xl">{`${data.rates[currTo]} ${currency[currTo]}`}</h1>
+                        <p>{data.date}</p>
+                    </div>
+                ) : (
+                    <h1>Select a currency you wanna count</h1>
+                )}
                 <CurrencyInput setValue={setCurrFrom} />
                 <CurrencyInput setValue={setCurrTo} />
             </div>
