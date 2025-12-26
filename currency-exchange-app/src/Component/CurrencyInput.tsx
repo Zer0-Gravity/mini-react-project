6d5a5a7e43ee3f2c6aed8e81060fc01f3aba64e9
Zer0-Gravity <muslikhulimam878@gmail.com>
2025-12-26 18:32:15 +0700

    Adding a flag icon and conditional search

--- currency-exchange-app/src/Component/CurrencyInput.tsx
+++ currency-exchange-app/src/Component/CurrencyInput.tsx
@@ -1,4 +1,42 @@
-import React, { useState, type Dispatch, type SetStateAction } from "react";
+import {
+    AU,
+    BG,
+    BR,
+    CA,
+    CH,
+    CN,
+    CZ,
+    DK,
+    EU,
+    GB,
+    HK,
+    HU,
+    ID,
+    IL,
+    IN,
+    IS,
+    JP,
+    KR,
+    MX,
+    MY,
+    NO,
+    NZ,
+    PH,
+    PL,
+    RO,
+    SE,
+    SG,
+    TH,
+    TR,
+    US,
+    ZA,
+} from "country-flag-icons/react/1x1";
+import React, {
+    useState,
+    type Dispatch,
+    type ReactElement,
+    type SetStateAction,
+} from "react";
 
 interface Input {
     setValue: Dispatch<SetStateAction<string>>;
@@ -9,44 +47,46 @@ interface Input {
 function CurrencyInput({ setValue, handleInput, amount }: Input) {
     const [isOpen, setIsOpen] = useState(false);
     const [currency, setCurrency] = useState<string>("");
+    const [flag, setFlag] = useState<ReactElement>();
     const [search, setSearch] = useState<string>("");
 
     const currencyData = [
-        "AUD",
-        "BGN",
-        "BRL",
-        "CAD",
-        "CHF",
-        "CNY",
-        "CZK",
-        "DKK",
-        "EUR",
-        "GBP",
-        "HKD",
-        "HUF",
-        "IDR",
-        "ILS",
-        "INR",
-        "ISK",
-        "JPY",
-        "KRW",
-        "MXN",
-        "MYR",
-        "NOK",
-        "NZD",
-        "PHP",
-        "PLN",
-        "RON",
-        "SEK",
-        "SGD",
-        "THB",
-        "TRY",
-        "USD",
-        "ZAR",
+        { currency: "AUD", flagCode: <AU className="w-7 rounded-full" /> },
+        { currency: "BGN", flagCode: <BG className="w-7 rounded-full" /> },
+        { currency: "BRL", flagCode: <BR className="w-7 rounded-full" /> },
+        { currency: "CAD", flagCode: <CA className="w-7 rounded-full" /> },
+        { currency: "CHF", flagCode: <CH className="w-7 rounded-full" /> },
+        { currency: "CNY", flagCode: <CN className="w-7 rounded-full" /> },
+        { currency: "CZK", flagCode: <CZ className="w-7 rounded-full" /> },
+        { currency: "DKK", flagCode: <DK className="w-7 rounded-full" /> },
+        { currency: "EUR", flagCode: <EU className="w-7 rounded-full" /> },
+        { currency: "GBP", flagCode: <GB className="w-7 rounded-full" /> },
+        { currency: "HKD", flagCode: <HK className="w-7 rounded-full" /> },
+        { currency: "HUF", flagCode: <HU className="w-7 rounded-full" /> },
+        { currency: "IDR", flagCode: <ID className="w-7 rounded-full" /> },
+        { currency: "ILS", flagCode: <IL className="w-7 rounded-full" /> },
+        { currency: "INR", flagCode: <IN className="w-7 rounded-full" /> },
+        { currency: "ISK", flagCode: <IS className="w-7 rounded-full" /> },
+        { currency: "JPY", flagCode: <JP className="w-7 rounded-full" /> },
+        { currency: "KRW", flagCode: <KR className="w-7 rounded-full" /> },
+        { currency: "MXN", flagCode: <MX className="w-7 rounded-full" /> },
+        { currency: "MYR", flagCode: <MY className="w-7 rounded-full" /> },
+        { currency: "NOK", flagCode: <NO className="w-7 rounded-full" /> },
+        { currency: "NZD", flagCode: <NZ className="w-7 rounded-full" /> },
+        { currency: "PHP", flagCode: <PH className="w-7 rounded-full" /> },
+        { currency: "PLN", flagCode: <PL className="w-7 rounded-full" /> },
+        { currency: "RON", flagCode: <RO className="w-7 rounded-full" /> },
+        { currency: "SEK", flagCode: <SE className="w-7 rounded-full" /> },
+        { currency: "SGD", flagCode: <SG className="w-7 rounded-full" /> },
+        { currency: "THB", flagCode: <TH className="w-7 rounded-full" /> },
+        { currency: "TRY", flagCode: <TR className="w-7 rounded-full" /> },
+        { currency: "USD", flagCode: <US className="w-7 rounded-full" /> },
+        { currency: "ZAR", flagCode: <ZA className="w-7 rounded-full" /> },
     ];
 
-    const handleValue = (currency: string) => {
+    const handleValue = (currency: string, flagCode: ReactElement) => {
         setCurrency(currency);
+        setFlag(flagCode);
         setValue(currency);
     };
 
@@ -54,8 +94,8 @@ function CurrencyInput({ setValue, handleInput, amount }: Input) {
         setSearch(e.target.value);
     };
 
-    const filteredItem = currencyData.filter((item) => {
-        return item.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    const filteredItem = currencyData.filter(({ currency }) => {
+        return currency.toLowerCase().indexOf(search.toLowerCase()) !== -1;
     });
 
     return (
@@ -70,10 +110,17 @@ function CurrencyInput({ setValue, handleInput, amount }: Input) {
             <div className="bg-white p-2 relative">
                 {/* Custom Dropdown button */}
                 <button onClick={() => setIsOpen(!isOpen)}>
-                    {currency || "Currency"}
+                    {currency ? (
+                        <div className="flex items-center gap-1">
+                            <h1>{currency}</h1>
+                            <span>{flag}</span>
+                        </div>
+                    ) : (
+                        "Currency"
+                    )}
                 </button>
                 {isOpen && (
-                    <div className="absolute mt-3 bg-gray-400 p-2">
+                    <div className="absolute mt-3 bg-gray-400 p-2 max-h-100 overflow-hidden overflow-y-auto">
                         <input
                             type="search"
                             placeholder="Search currency"
@@ -85,14 +132,24 @@ function CurrencyInput({ setValue, handleInput, amount }: Input) {
                             className="cursor-pointer"
                             onClick={() => setIsOpen(false)}
                         >
-                            {filteredItem.map((item, index) => (
-                                <li
-                                    key={index}
-                                    onClick={() => handleValue(item)}
-                                >
-                                    {item}
-                                </li>
-                            ))}
+                            {filteredItem.length > 0 ? (
+                                filteredItem.map((item, index) => (
+                                    <label
+                                        className="flex justify-between p-2 hover:bg-amber-200"
+                                        onClick={() =>
+                                            handleValue(
+                                                item.currency,
+                                                item.flagCode
+                                            )
+                                        }
+                                    >
+                                        <li key={index}>{item.currency}</li>
+                                        <span>{item.flagCode}</span>
+                                    </label>
+                                ))
+                            ) : (
+                                <p>Country not found</p>
+                            )}
                         </ul>
                     </div>
                 )}
