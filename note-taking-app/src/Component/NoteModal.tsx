c6c16064d8c6187e638dc2de0e84a0924fb1578c
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-13 08:01:51 +0700

    Fix: Note display html syntax instead a full structured text

--- note-taking-app/src/Component/NoteModal.tsx
+++ note-taking-app/src/Component/NoteModal.tsx
@@ -20,27 +20,41 @@ function NoteModal({
     const bodyRef = useRef<HTMLDivElement>(null);
 
     const handleSave = () => {
-        const title = titleRef.current?.innerHTML ?? "";
+        const title = titleRef.current?.innerText ?? "";
         const body = bodyRef.current?.innerHTML ?? "";
 
         const noteData = {
             documentId: isEdit ? selectedNote : crypto.randomUUID(),
-            title: title,
+            title: title ? title : "Untitled",
             body: body,
         };
 
+        if (!body) {
+            alert("Note can't be empty");
+            return;
+        }
         saveNote(noteData);
     };
 
     return (
         <div className="absolute inset-0 flex items-center justify-center bg-black/50">
             <div className="bg-white">
-                <div contentEditable ref={titleRef}>
-                    {isEdit ? activeNote?.title : "Untitled"}
-                </div>
-                <div contentEditable ref={bodyRef}>
-                    {isEdit ? activeNote?.body : ""}
-                </div>
+                <div
+                    key={activeNote?.documentId}
+                    contentEditable
+                    ref={titleRef}
+                    data-placeholder={isEdit ? activeNote?.title : "Untitled"}
+                    className="title-editor"
+                ></div>
+                <div
+                    key={activeNote?.documentId}
+                    contentEditable
+                    ref={bodyRef}
+                    dangerouslySetInnerHTML={{
+                        __html: (isEdit ? activeNote?.body : "") ?? "",
+                    }}
+                    className="body-editor"
+                ></div>
                 <div>
                     <button onClick={onClose}>Close</button>
                     <button onClick={handleSave}>Save</button>
