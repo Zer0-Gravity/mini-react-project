d6c1fcfb9017ab3a852113ff3a63bc9dd5cf4807
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-14 08:20:21 +0700

    Feat: Style modal for new collection form

--- note-taking-app/src/Component/Modal.tsx
+++ note-taking-app/src/Component/Modal.tsx
@@ -1,4 +1,5 @@
 import type { Dispatch, SetStateAction } from "react";
+import { IoMdCloseCircle } from "react-icons/io";
 
 interface ModalProps {
     onClose: () => void;
@@ -22,21 +23,42 @@ function Modal({ onClose, inputHandler }: ModalProps) {
 
     return (
         <div className="absolute bg-black/50 inset-0 flex items-center justify-center top-0">
-            <div className="bg-white flex flex-col">
-                <h1>Add new collection</h1>
+            <div className="bg-primary-color flex flex-col w-[500px] gap-3 p-5 rounded-lg">
+                <div className="flex justify-between">
+                    <h1 className="text-2xl font-semibold">
+                        Add new collection
+                    </h1>
+                    <IoMdCloseCircle
+                        size={25}
+                        onClick={onClose}
+                        color="white
+                    "
+                    />
+                </div>
                 <input
                     type="text"
                     placeholder="Collection name..."
                     onChange={handleTitleCollection}
+                    className="outline-none p-2 bg-third-color rounded-lg"
                 />
                 <textarea
                     placeholder="Add description"
-                    className="resize-none"
+                    className="resize-none outline-none bg-third-color h-20 p-2 rounded-lg"
                     onChange={handleDescCollection}
                 />
-                <div className="flex-">
-                    <button onClick={onClose}>Cancel</button>
-                    <button onClick={inputHandler.addNewCollection}>Add</button>
+                <div className="flex justify-end gap-2">
+                    <button
+                        onClick={onClose}
+                        className="w-20 py-1.5 rounded-lg bg-secondary-color"
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        onClick={inputHandler.addNewCollection}
+                        className="w-20 py-1.5 rounded-lg bg-secondary-color"
+                    >
+                        Add
+                    </button>
                 </div>
             </div>
         </div>
