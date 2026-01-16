932648ab55d9a23a74e0908a52e1c89364597cbb
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-16 13:11:28 +0700

    Feat: Add tab logic behavior on forecast and favorite button

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,24 +1,44 @@
+import type React from "react";
 import WeatherDisplay from "./Component/WeatherDisplay";
 import FavoriteList from "./PanelTab/FavoriteList";
 import WeekForecast from "./PanelTab/WeekForecast";
+import { useState } from "react";
 
 function App() {
+    const [selectedTab, setSelectedTab] = useState<string>("forecast");
+
+    const handleTabChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedTab(e.target.value);
+    };
+
     return (
         <div className="flex">
             <WeatherDisplay />
             <div>
                 <div>
                     <label>
-                        <input type="radio" name="navigation-tab" />
+                        <input
+                            type="radio"
+                            name="navigation-tab"
+                            value="forecast"
+                            checked={selectedTab === "forecast"}
+                            onChange={handleTabChange}
+                        />
                         <span>Week Forecast</span>
                     </label>
                     <label>
-                        <input type="radio" name="navigation-tab" />
+                        <input
+                            type="radio"
+                            name="navigation-tab"
+                            value="favorite"
+                            checked={selectedTab === "favorite"}
+                            onChange={handleTabChange}
+                        />
                         <span>Favorite</span>
                     </label>
                 </div>
-                <WeekForecast />
-                <FavoriteList />
+                {selectedTab === "forecast" && <WeekForecast />}
+                {selectedTab === "favorite" && <FavoriteList />}
             </div>
         </div>
     );
