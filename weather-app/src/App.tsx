8c12c4928119cfd0a8e3c403ad14497759600654
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-16 13:41:42 +0700

    Feat: Fetch Open Meteo weather

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -2,15 +2,33 @@ import type React from "react";
 import WeatherDisplay from "./Component/WeatherDisplay";
 import FavoriteList from "./PanelTab/FavoriteList";
 import WeekForecast from "./PanelTab/WeekForecast";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TabController from "./Component/TabController";
+import axios from "axios";
 
 function App() {
     const [selectedTab, setSelectedTab] = useState<string>("forecast");
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(() => {
+        async function fetchWeather() {
+            await axios
+                .get(
+                    "https://api.open-meteo.com/v1/forecast?latitude=34.6938&longitude=135.5011&daily=uv_index_max,temperature_2m_max,temperature_2m_min,weather_code&current=temperature_2m,weather_code,precipitation,relative_humidity_2m,wind_speed_10m&timezone=GMT"
+                )
+                .then((response) => {
+                    setWeatherData(response.data);
+                });
+        }
+
+        fetchWeather();
+    }, []);
 
     const handleTabChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSelectedTab(e.target.value);
     };
+
+    console.log(weatherData);
 
     return (
         <main className="flex">
