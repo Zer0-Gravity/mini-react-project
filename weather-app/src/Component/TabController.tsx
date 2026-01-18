febc97f57dffc1238b275d65a311560a763bec3a
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-18 14:55:53 +0700

    Feat: Style Nav button

--- weather-app/src/Component/TabController.tsx
+++ weather-app/src/Component/TabController.tsx
@@ -7,26 +7,28 @@ interface TabControllerProps {
 
 function TabController({ handleTabChange, selectedTab }: TabControllerProps) {
     return (
-        <div>
-            <label>
+        <div className="border-2 border-border-color w-105 h-12 p-1 flex justify-between items-center bg-primary-color rounded-full">
+            <label className="rounded-full h-8.75 w-49 flex items-center justify-center has-checked:bg-border-color">
                 <input
                     type="radio"
                     name="navigation-tab"
                     value="forecast"
                     checked={selectedTab === "forecast"}
                     onChange={handleTabChange}
+                    className="hidden"
                 />
-                <span>Week Forecast</span>
+                <span className="font-semibold">Week Forecast</span>
             </label>
-            <label>
+            <label className="rounded-full h-8.75 w-49 flex items-center justify-center has-checked:bg-border-color">
                 <input
                     type="radio"
                     name="navigation-tab"
                     value="favorite"
                     checked={selectedTab === "favorite"}
                     onChange={handleTabChange}
+                    className="hidden"
                 />
-                <span>Favorite</span>
+                <span className="font-semibold">Favorite</span>
             </label>
         </div>
     );
