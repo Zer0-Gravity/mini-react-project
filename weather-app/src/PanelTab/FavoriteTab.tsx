2f80454e99db95ab4b66abccfd9e75777d77fc1a
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-19 11:59:52 +0700

    Feat: Add card and style favorite tab

--- weather-app/src/PanelTab/FavoriteTab.tsx
+++ weather-app/src/PanelTab/FavoriteTab.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import type { CombinedData, FavoriteList } from "../Utils/Type";
 import axios from "axios";
 import { weatherMapping } from "../Utils/WeatherCode";
-import { LucideLoader } from "lucide-react";
+import { LucideHeart, LucideLoader } from "lucide-react";
 
 interface FavoriteListProps {
     favorite: FavoriteList[];
@@ -28,7 +28,7 @@ function FavoriteTab({ favorite }: FavoriteListProps) {
                             current: `weather_code,temperature_2m,relative_humidity_2m`,
                             timezone: `auto`,
                         },
-                    }
+                    },
                 );
                 const cityWeather: CombinedData[] = response.data;
 
@@ -49,11 +49,13 @@ function FavoriteTab({ favorite }: FavoriteListProps) {
     }, [lats, lons, favorite]);
 
     return (
-        <div>
-            <header>
-                <h1>Favorite</h1>
+        <div className="border-2 border-border-color bg-primary-color rounded-[20px] h-143.75 overflow-y-auto scrollbar">
+            <header className="sticky top-0 bg-primary-color border-b-2 border-border-color">
+                <h1 className="text-[30px] font-semibold py-2 px-5 text-center">
+                    Favorite
+                </h1>
             </header>
-            <section>
+            <section className="flex flex-col gap-4 p-3">
                 {favData ? (
                     favData.map((data) => {
                         const weatherCode = data.current.weather_code ?? 0;
@@ -63,15 +65,19 @@ function FavoriteTab({ favorite }: FavoriteListProps) {
                         };
 
                         return (
-                            <div key={data.name}>
-                                <div>
-                                    <img
-                                        src="https://placehold.co/30x30"
-                                        alt="icon"
+                            <div
+                                key={data.name}
+                                className="px-4 rounded-lg bg-primary-color border border-border-color flex items-center justify-between"
+                            >
+                                <div className="flex items-center gap-2">
+                                    <LucideHeart
+                                        size={20}
+                                        fill="red"
+                                        stroke="red"
                                     />
-                                    <h1>{data.name}</h1>
+                                    <h1 className="text-[15px]">{data.name}</h1>
                                 </div>
-                                <div>
+                                <div className="flex items-center gap-2">
                                     <img
                                         src={`/Weather Icon/${weather.icon}.svg`}
                                         alt="weather"
@@ -79,9 +85,9 @@ function FavoriteTab({ favorite }: FavoriteListProps) {
                                     />
                                     <span>{weather.label}</span>
                                 </div>
-                                <div>
+                                <div className="flex items-center gap-2">
                                     <h1>{data.current.temperature_2m}°</h1>
-                                    <h1>
+                                    <h1 className="text-gray-400">
                                         {data.current.relative_humidity_2m}%
                                     </h1>
                                 </div>
@@ -89,7 +95,7 @@ function FavoriteTab({ favorite }: FavoriteListProps) {
                         );
                     })
                 ) : (
-                    <div>No data</div>
+                    <div className="text-center font-bold">No data</div>
                 )}
                 {isLoading && (
                     <div>
