de54325fe114f62681310b1d4baebcf8acc33972
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-31 18:32:36 +0700

    Feat: Add history transaction skeleton html

--- finance-tracker/src/Pages/History.tsx
+++ finance-tracker/src/Pages/History.tsx
@@ -1,5 +1,27 @@
+import { Filter } from "lucide-react";
+import TrackerCard from "../Component/TrackerCard";
+
 function History() {
-    return <div className="container-custom">History Transaction</div>;
+    return (
+        <div className="container-custom">
+            <h1 className="font-bold text-[25px] text-secondary">
+                History Transaction
+            </h1>
+            <section>
+                <input type="search" placeholder="Search Transaction" />
+                <div className="flex text-secondary">
+                    <h1>
+                        <Filter /> Filter
+                    </h1>
+                </div>
+            </section>
+            <section className="space-y-1">
+                <TrackerCard />
+                <TrackerCard />
+                <TrackerCard />
+            </section>
+        </div>
+    );
 }
 
 export default History;
