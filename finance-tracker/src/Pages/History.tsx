8929263c15c074c918d0c947e396e0798205010c
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-31 18:54:23 +0700

    Feat: add react select for filter option

--- finance-tracker/src/Pages/History.tsx
+++ finance-tracker/src/Pages/History.tsx
@@ -1,7 +1,26 @@
 import { Filter } from "lucide-react";
 import TrackerCard from "../Component/TrackerCard";
+import { useState } from "react";
+import type { SingleValue } from "react-select";
+import ReactSelect from "react-select";
+
+interface TransactionType {
+    value: string;
+    label: string;
+}
 
 function History() {
+    const [selectedOption, setSelectedOption] =
+        useState<TransactionType | null>(null);
+    const options: TransactionType[] = [
+        { value: "income", label: "Income" },
+        { value: "Expense", label: "Expense" },
+    ];
+
+    const handleChange = (newValue: SingleValue<TransactionType>) => {
+        setSelectedOption(newValue);
+    };
+
     return (
         <div className="container-custom">
             <h1 className="font-bold text-[25px] text-secondary">
@@ -13,6 +32,11 @@ function History() {
                     <h1>
                         <Filter /> Filter
                     </h1>
+                    <ReactSelect<TransactionType>
+                        options={options}
+                        value={selectedOption}
+                        onChange={handleChange}
+                    />
                 </div>
             </section>
             <section className="space-y-1">
