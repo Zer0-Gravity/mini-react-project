56936ba222d186a91a79b5dc22a29baf3e93b9e8
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-31 08:51:37 +0700

    Style: Add and style html for balance display

--- finance-tracker/src/Pages/Home.tsx
+++ finance-tracker/src/Pages/Home.tsx
@@ -1,5 +1,36 @@
+import { MailPlus, Send, type LucideIcon } from "lucide-react";
+
 function Home() {
-    return <div className="container-custom">Home</div>;
+    return (
+        <main className="container-custom">
+            <section className="w-full h-67.5 bg-secondary p-2 rounded-lg flex-col flex items-center justify-center gap-3">
+                <h1 className="text-[16px] font-medium">Your Balance</h1>
+                <h1 className="text-[50px] font-medium">$450,000.00</h1>
+                <div className="flex gap-20">
+                    <Button text="Send Money" icon={Send} />
+                    <Button text="Receive Money" icon={MailPlus} />
+                </div>
+            </section>
+
+            <section></section>
+        </main>
+    );
+}
+
+interface ButtonProps {
+    icon: LucideIcon;
+    text: string;
+}
+
+function Button({ icon: Icon, text }: ButtonProps) {
+    return (
+        <div className="flex flex-col items-center gap-2">
+            <button className="w-12.5 h-12.5 rounded-full bg-button flex items-center justify-center">
+                {<Icon size={20} color="white" />}
+            </button>
+            <span className="font-medium text-primary">{text}</span>
+        </div>
+    );
 }
 
 export default Home;
