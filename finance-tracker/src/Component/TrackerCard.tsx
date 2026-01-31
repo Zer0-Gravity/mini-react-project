df9b899fe3a884bfcfdbc4f3e0c54d5f0dd1ce7d
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-31 11:44:15 +0700

    Style: Transaction/Expense list card

--- finance-tracker/src/Component/TrackerCard.tsx
+++ finance-tracker/src/Component/TrackerCard.tsx
@@ -2,22 +2,24 @@ import { MoveUpRight, Trash } from "lucide-react";
 
 function TrackerCard() {
     return (
-        <div className="text-white border flex justify-between p-2">
-            <div>
-                <div>
+        <div className="text-white border flex justify-between p-2 bg-secondary items-center rounded-lg">
+            <div className="flex gap-5 items-center">
+                <div className="border w-10 h-10 flex items-center justify-center rounded-full">
                     <MoveUpRight />
                 </div>
-                <div>
-                    <h1>Gift from grandma</h1>
-                    <p>Today, 15.00PM</p>
+                <div className="text-primary">
+                    <h1 className="font-bold">Gift from grandma</h1>
+                    <p className="text-[14px] italic">Today, 15.00PM</p>
                 </div>
             </div>
 
-            <div className="flex gap-5">
-                <p>Income</p>
-                <h1>$23.00</h1>
+            <div className="flex gap-10 text-primary items-center">
+                <p className="border-2 px-2 py-1 rounded-sm text-green-400 font-medium">
+                    Income
+                </p>
+                <h1 className="font-bold text-[18px]">$23.00</h1>
                 <button>
-                    <Trash />
+                    <Trash size={20} />
                 </button>
             </div>
         </div>
