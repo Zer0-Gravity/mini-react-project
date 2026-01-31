c7af0e3b4780937ef59bae0ac66b8d58c32d6701
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-01-31 09:22:42 +0700

    Style: Add and Style button add html Feat: Make ButtonAdd Component

---
+++ finance-tracker/src/Component/Home/ButtonAdd.tsx
@@ -0,0 +1,19 @@
+import type { LucideIcon } from "lucide-react";
+
+interface ButtonProps {
+    icon: LucideIcon;
+    text: string;
+}
+
+function ButtonAdd({ icon: Icon, text }: ButtonProps) {
+    return (
+        <>
+            <button className="flex gap-2 text-secondary hover:border-b-2">
+                <Icon size={20} />
+                <span className="text-[14px] font-medium">{text}</span>
+            </button>
+        </>
+    );
+}
+
+export default ButtonAdd;
--- finance-tracker/src/Pages/Home.tsx
+++ finance-tracker/src/Pages/Home.tsx
@@ -1,8 +1,17 @@
-import { MailPlus, Send, type LucideIcon } from "lucide-react";
+import {
+    Goal,
+    HandCoins,
+    MailPlus,
+    Send,
+    ShoppingBag,
+    type LucideIcon,
+} from "lucide-react";
+import ButtonAdd from "../Component/Home/ButtonAdd";
 
 function Home() {
     return (
-        <main className="container-custom">
+        <main className="container-custom flex flex-col gap-5">
+            {/* balance Display */}
             <section className="w-full h-67.5 bg-secondary p-2 rounded-lg flex-col flex items-center justify-center gap-3">
                 <h1 className="text-[16px] font-medium">Your Balance</h1>
                 <h1 className="text-[50px] font-medium">$450,000.00</h1>
@@ -12,7 +21,31 @@ function Home() {
                 </div>
             </section>
 
-            <section></section>
+            {/* Add button */}
+            <section>
+                <div className="flex justify-end gap-5">
+                    <ButtonAdd icon={Goal} text="Add Goal" />
+                    <ButtonAdd icon={HandCoins} text="Add Income" />
+                    <ButtonAdd icon={ShoppingBag} text="Add Expense" />
+                </div>
+            </section>
+
+            <section>
+
+                {/* Goal card */}
+                <div>
+                    <h1 className="text-[16px] font-semibold text-secondary">
+                        Goal
+                    </h1>
+                </div>
+
+                {/* Activity Card */}
+                <div>
+                    <h1 className="text-[16px] font-semibold text-secondary">
+                        Recent Activity
+                    </h1>
+                </div>
+            </section>
         </main>
     );
 }
