d99bbec8ee1f4337aedee3c6a3f1919d91964067
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-03-04 10:30:12 +0700

    Feat: Add responsive registration page

--- user-form/src/App.tsx
+++ user-form/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./Component/Login/Login";
+import Register from "./Component/Register/Register";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ user-form/src/Component/Register/Register.tsx
@@ -0,0 +1,76 @@
+import { Eye, LockKeyhole, Mail, UserRound } from "lucide-react";
+import { Link } from "react-router";
+
+function Register() {
+    return (
+        <main className="min-h-screen w-full flex items-center justify-center">
+            <section className="absolute inset-0 md:relative md:w-100 md:h-125">
+                <img
+                    src="/beach.jpg"
+                    alt="beach"
+                    className="object-cover h-full w-full md:rounded-l-2xl"
+                />
+            </section>
+            <section className="relative z-10 h-screen w-full bg-white/20 flex flex-col justify-center backdrop-blur-[5px] md:w-100 md:h-125 md:bg-primary md:rounded-r-2xl ">
+                <div className="mx-auto md:mx-10 space-y-3 min-w-75">
+                    <h1 className="text-[30px] font-extrabold">Register</h1>
+                    <form className="space-y-6">
+                        <div className="border-b-2 flex gap-2 items-center">
+                            <UserRound className="w-5" />
+                            <input
+                                type="text"
+                                placeholder="First Name"
+                                className="w-full py-2 outline-none autofill:bg-transparent"
+                            />
+                        </div>
+                        <div className="border-b-2 flex gap-2 items-center">
+                            <UserRound className="w-5" />
+                            <input
+                                type="text"
+                                placeholder="Last Name"
+                                className="w-full py-2 outline-none autofill:bg-transparent"
+                            />
+                        </div>
+                        <div className="border-b-2 flex gap-2 items-center">
+                            <Mail className="w-5" />
+                            <input
+                                type="text"
+                                placeholder="Email"
+                                className="w-full py-2 outline-none autofill:bg-transparent"
+                            />
+                        </div>
+                        <div className="border-b-2 flex gap-2 items-center">
+                            <LockKeyhole className="w-5" />
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="flex-1 py-2 outline-none"
+                            />
+                            <Eye className="w-5" />
+                        </div>
+                        <div className="border-b-2 flex gap-2 items-center">
+                            <LockKeyhole className="w-5" />
+                            <input
+                                type="password"
+                                placeholder="Verify Password"
+                                className="flex-1 py-2 outline-none"
+                            />
+                            <Eye className="w-5" />
+                        </div>
+                    </form>
+                    <p className="text-small">
+                        Don't have account yet?
+                        <Link to="/" className="text-secondary underline">
+                            Sign In
+                        </Link>
+                    </p>
+                    <button className="w-full bg-button h-12.5 text-white rounded-lg text-big font-bold">
+                        Sign In
+                    </button>
+                </div>
+            </section>
+        </main>
+    );
+}
+
+export default Register;
