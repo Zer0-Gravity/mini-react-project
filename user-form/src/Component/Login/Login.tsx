fba6cd1106bab82044fc4e90147e5409352fa0e6
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-03-06 11:43:34 +0700

    Feat: Use react hook form for the input form, and render error input

--- user-form/src/Component/Login/Login.tsx
+++ user-form/src/Component/Login/Login.tsx
@@ -1,9 +1,21 @@
-import { LockKeyhole, Mail } from "lucide-react";
+import { AlertCircle, LockKeyhole, Mail } from "lucide-react";
 import { Link } from "react-router";
 import ButtonPassword from "../Utils/ButtonPassword";
-import { useTogglePassword } from "../Utils/function";
+import {
+    EMAIL_REGEX,
+    PASSWORD_REGEX,
+    useTogglePassword,
+} from "../Utils/function";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Login() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({ mode: "onChange" });
+
     return (
         <main className="min-h-screen w-full flex items-center justify-center">
             <section className="absolute inset-0 md:relative md:w-100 md:h-125">
@@ -22,43 +34,83 @@ function Login() {
                         Welcome Back!!
                     </h1>
                     <form className="space-y-6">
-                        <div className="border-b-2 flex gap-2 items-center">
+                        <div
+                            className={`border-b-2 flex gap-2 items-center relative ${errors.email ? "border-red-500" : "border-black"}`}
+                        >
                             <Mail className="w-5" />
                             <input
-                                type="text"
+                                {...register("email", {
+                                    required: "Email is required",
+                                    pattern: {
+                                        value: EMAIL_REGEX,
+                                        message: "Please enter valid email",
+                                    },
+                                })}
+                                className="flex-1 py-2 outline-none"
                                 placeholder="Email"
-                                className="w-full py-2 outline-none autofill:bg-transparent"
                             />
+                            {errors.email?.message && (
+                                <>
+                                    <AlertCircle className="text-red-500 w-5" />
+                                    <p className="text-small absolute -bottom-5.5 right-0 text-red-500">
+                                        {errors.email?.message as string}
+                                    </p>
+                                </>
+                            )}
                         </div>
-                        <div className="border-b-2 flex gap-2 items-center">
+                        <div
+                            className={`border-b-2 flex gap-2 items-center relative ${errors.password ? "border-red-500" : "border-black"}`}
+                        >
                             <LockKeyhole className="w-5" />
                             <input
+                                {...register("password", {
+                                    required: "Password is required",
+                                    pattern: {
+                                        value: PASSWORD_REGEX,
+                                        message:
+                                            "Password need at least 8 character length, have an uppercase, symbol, and number",
+                                    },
+                                })}
                                 type={useTogglePassword()}
-                                placeholder="Password"
                                 className="flex-1 py-2 outline-none"
+                                placeholder="Email"
                             />
+                            {errors.password && (
+                                <p className="text-small absolute -bottom-5.5 right-0 text-red-500">
+                                    {errors.password?.message as string}
+                                </p>
+                            )}
                             <ButtonPassword />
                         </div>
-                    </form>
-                    <div className="space-y-2">
-                        <p className="text-center text-small">or</p>
-                        <button className="bg-white h-12.5 rounded-lg flex text-med items-center justify-center w-full">
-                            <img src="/Google-icon.svg" className="w-5 mr-3" />
-                            Sign in with Google
-                        </button>
-                    </div>
-                    <p className="text-small">
-                        Don't have account yet?
-                        <Link
-                            to="/register"
-                            className="text-secondary underline"
+                        <div className="space-y-2">
+                            <p className="text-center text-small">or</p>
+                            <button
+                                type="button"
+                                className="bg-white h-12.5 rounded-lg flex text-med items-center justify-center w-full"
+                            >
+                                <img
+                                    src="/Google-icon.svg"
+                                    className="w-5 mr-3"
+                                />
+                                Sign in with Google
+                            </button>
+                        </div>
+                        <p className="text-small">
+                            Don't have account yet?
+                            <Link
+                                to="/register"
+                                className="text-secondary underline"
+                            >
+                                Sign Up
+                            </Link>
+                        </p>
+                        <button
+                            type="submit"
+                            className="w-full bg-button h-12.5 text-white rounded-lg text-med font-bold"
                         >
-                            Sign Up
-                        </Link>
-                    </p>
-                    <button className="w-full bg-button h-12.5 text-white rounded-lg text-med font-bold">
-                        SIGN IN
-                    </button>
+                            SIGN IN
+                        </button>
+                    </form>
                 </div>
             </section>
         </main>
--- user-form/src/Component/Utils/function.ts
+++ user-form/src/Component/Utils/function.ts
@@ -1,5 +1,9 @@
 import { usePassword } from "../../store";
 
+export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const PASSWORD_REGEX =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
 export function useTogglePassword(): string {
     const { togglePass } = usePassword();
 
