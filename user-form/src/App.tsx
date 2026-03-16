a1702f9aa6fecef27cf399bc875bb34319f8caf1
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-03-16 13:13:30 +0700

    Feat: add refresh token fetch on the page

--- user-form/src/App.tsx
+++ user-form/src/App.tsx
@@ -4,8 +4,30 @@ import Register from "./Component/Register/Register";
 import RegisterSuccess from "./Component/Register/RegisterSuccess";
 import LoginSuccess from "./Component/Login/LoginSuccess";
 import ProtectedRoute from "./Component/Utils/ProtectedRoute";
+import { useEffect } from "react";
+import axios from "axios";
+import { useAuthUser } from "./store";
 
 function App() {
+    const { setUser } = useAuthUser();
+
+    //verify the refresh token if it exist
+    useEffect(() => {
+        const verifyRefreshToken = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:3500/api/refresh",
+                    { withCredentials: true },
+                );
+                setUser(response.data);
+            } catch (error) {
+                console.log("Error occurred", error);
+            }
+        };
+
+        verifyRefreshToken();
+    }, [setUser]);
+
     return (
         <BrowserRouter>
             <Routes>
