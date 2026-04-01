0b0f488de35bc32b2b70e9d4c3f15334b3b500fd
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-01 11:54:15 +0700

    Feat: Add functionality to add custom name

--- chat-app/chat-ui/src/Component/Chat.tsx
+++ chat-app/chat-ui/src/Component/Chat.tsx
@@ -1,9 +1,13 @@
 import { CircleX, Send, Settings, UserCircle2 } from "lucide-react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function Chat() {
     const [modal, setModal] = useState<boolean>(false);
+    const [username, setUsername] = useState<string>("");
 
+    const usernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
     return (
         <main className="flex flex-col h-screen w-screen md:w-200 pt-10 pb-10 text-white relative">
             <div className="flex justify-between items-center">
@@ -14,21 +18,21 @@ function Chat() {
             {modal && (
                 <main className="absolute inset-0 bg-black/5 flex items-center justify-center">
                     <section className="w-80 flex flex-col justify-between p-3 bg-secondary space-y-2 rounded-lg">
-                        <header className="flex justify-between items-center w-full">
-                            <h1 className="font-bold rounded-lg">Setting</h1>
-                            <CircleX
-                                onClick={() => setModal(false)}
-                                size={20}
-                            />
-                        </header>
+                        <h1 className="font-bold rounded-lg">Setting</h1>
+
                         <div className="space-y-5">
                             <label htmlFor="inputName">Change username</label>
                             <input
                                 type="text"
                                 id="inputName"
-                                className="border-primary border px-2 py-1 outline-none w-full"
+                                value={username}
+                                onChange={usernameChange}
+                                className=" px-2 py-1 outline-none w-full"
                             />
-                            <button className="flex justify-end bg-green-400 px-3 py-1 rounded-xs">
+                            <button
+                                onClick={() => setModal(false)}
+                                className="flex justify-end bg-green-400 px-3 py-1 rounded-xs"
+                            >
                                 Save
                             </button>
                         </div>
@@ -36,14 +40,16 @@ function Chat() {
                 </main>
             )}
 
-            <h2 className="text-xl text-center mt-5">Hello, Guest!!</h2>
+            <h2 className="text-xl text-center mt-5">
+                Hello, {username || "Guest"}!!
+            </h2>
 
             <section className="flex-1 flex flex-col justify-end p-2">
                 <div className="flex gap-5 ">
                     <UserCircle2 />
                     <div className="flex flex-col gap-1 p-2 min-w-70">
                         <div className="flex justify-between font-semibold text-red-500">
-                            <p>Guest</p>
+                            <p>{username || "Guest"}</p>
                             <p>16:20</p>
                         </div>
                         <p>
