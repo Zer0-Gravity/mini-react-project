e7fd0a382a1ca5af7680ca9ecd1b1806fbd2cfca
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-02 10:36:03 +0700

    Feat: Delete current chat design for re-design it

--- chat-app/chat-ui/src/Component/Chat.tsx
+++ chat-app/chat-ui/src/Component/Chat.tsx
@@ -1,64 +1,14 @@
-import { Send, Settings, UserCircle2 } from "lucide-react";
-import React, { useState } from "react";
+import { Send } from "lucide-react";
 
 function Chat() {
-    const [modal, setModal] = useState<boolean>(false);
-    const [username, setUsername] = useState<string>("");
-
-    const usernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value);
-    };
     return (
-        <main className="flex flex-col h-screen w-screen md:w-200 pt-10 pb-10 text-white relative">
-            <div className="flex justify-between items-center">
-                <h1 className="font-bold text-[25px]">Socket.io Group Chat</h1>
-                <Settings onClick={() => setModal(!modal)} />
-            </div>  
-
-            {modal && (
-                <main className="absolute inset-0 bg-black/5 flex items-center justify-center">
-                    <section className="w-80 flex flex-col justify-between p-3 bg-secondary space-y-2 rounded-lg">
-                        <h1 className="font-bold rounded-lg">Setting</h1>
-
-                        <div className="space-y-5">
-                            <label htmlFor="inputName">Change username</label>
-                            <input
-                                type="text"
-                                id="inputName"
-                                value={username}
-                                onChange={usernameChange}
-                                className=" px-2 py-1 outline-none w-full"
-                            />
-                            <button
-                                onClick={() => setModal(false)}
-                                className="flex justify-end bg-green-400 px-3 py-1 rounded-xs"
-                            >
-                                Save
-                            </button>
-                        </div>
-                    </section>
-                </main>
-            )}
-
-            <h2 className="text-xl text-center mt-5">
-                Hello, {username || "Guest"}!!
-            </h2>
-
+        <main className="flex flex-col h-screen w-screen md:w-200 pt-10  text-white relative">
             <section className="flex-1 flex flex-col justify-end p-2">
-                <div className="flex gap-5 ">
-                    <UserCircle2 />
-                    <div className="flex flex-col gap-1 p-2 min-w-70">
-                        <div className="flex justify-between font-semibold text-red-500">
-                            <p>{username || "Guest"}</p>
-                            <p>16:20</p>
-                        </div>
-                        <p>
-                            Hello this is message that i work on, who are you
-                            today is good day, doesnt it huehhue so technically
-                            iam free you know?
-                        </p>
-                    </div>
-                </div>
+                <p>
+                    Hello this is message that i work on, who are you today is
+                    good day, doesnt it huehhue so technically iam free you
+                    know?
+                </p>
             </section>
 
             <section className="bg-secondary p-4 md:rounded-lg w-full flex justify-between mt-10 gap-2 outline-none">
