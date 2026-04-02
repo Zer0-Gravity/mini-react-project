1842abe70b8316bd1a63aa549256dae9fe0d691e
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-02 15:47:14 +0700

    FeatL Add react router to App

--- chat-app/chat-ui/src/App.tsx
+++ chat-app/chat-ui/src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import Chat from "./Component/Chat";
 import Home from "./Component/Home";
 
 function App() {
     return (
-        <main className="bg-primary h-screen w-screen flex justify-center items-center">
-            <Home />
-            {/* <Chat />; */}
-        </main>
+        <BrowserRouter>
+            <main className="bg-primary h-screen w-screen flex justify-center items-center">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/public" element={<Chat />} />
+                </Routes>
+            </main>
+        </BrowserRouter>
     );
 }
 
