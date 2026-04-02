e77df4147804d73ba085854a051693db0088698b
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-02 10:01:30 +0700

    Feat: chat client connection to the backend using socket.io

--- chat-app/chat-backend/src/server.ts
+++ chat-app/chat-backend/src/server.ts
@@ -1,1 +1,29 @@
+import type { Application } from "express";
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import cors from "cors";
 
+const app: Application = express();
+const PORT = process.env.PORT || 3500;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: "http://localhost:5173" },
+});
+
+app.use(cors);
+
+io.on("connection", (socket) => {
+    console.log(`User Connected : ${socket.id}`);
+
+    socket.on("send message", (data) => {
+        io.emit("receive message", data);
+    });
+
+    socket.on("disconnected", () => {
+        console.log(`User disconnected : ${socket.id}`);
+    });
+});
+
+app.listen(PORT, () => console.log(`Server run in port ${PORT}`));
