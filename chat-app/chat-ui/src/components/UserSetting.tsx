6555cba405e32cfdf10966b919383cc234f1700a
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-22 08:59:12 +0700

    feat: add notification UI every time user save the data
    - Delete download data button

--- chat-app/chat-ui/src/components/UserSetting.tsx
+++ chat-app/chat-ui/src/components/UserSetting.tsx
@@ -1,5 +1,5 @@
-import { Copy, Download, Trash, XCircle } from "lucide-react";
-import { useState } from "react";
+import { Copy, Download, Save, Trash, XCircle } from "lucide-react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router";
 import type { UserProfile } from "../type";
 import { randomId } from "../utils";
@@ -9,8 +9,10 @@ import WarningMessage from "./WarningMessage";
 function UserSetting() {
     const navigate = useNavigate();
     const { addUser, userData } = useUserData();
+    const timeNotificationRef = useRef<ReturnType<typeof setTimeout>>(null);
     const [displayName, setDisplayName] = useState<string>("");
     const [warn, setWarn] = useState<boolean>(false);
+    const [notification, setNotification] = useState<boolean>(false);
 
     console.log(userData);
 
@@ -25,10 +27,33 @@ function UserSetting() {
 
         //Add data to the object
         addUser(newUser);
+
+        handleNotification();
+    };
+
+    const handleNotification = () => {
+        //Clear timeout upon unmount
+        if (timeNotificationRef.current) {
+            clearTimeout(timeNotificationRef.current);
+        }
+
+        //Set notification to true
+        setNotification(true);
+
+        //Timeout at 1 second
+        timeNotificationRef.current = setTimeout(() => {
+            setNotification(false);
+        }, 1000);
     };
 
     return (
         <main className="bg-main-chat h-screen w-screen p-5 flex justify-center text-primary-text relative">
+            {notification && (
+                <div className="absolute bg-green-300 rounded-sm text-black flex gap-2 p-2 border-l-4 border-green-500 ">
+                    <Save />
+                    <p>Data saved!</p>
+                </div>
+            )}
             {warn && <WarningMessage setWarn={setWarn} />}
             <div className="md:w-165 w-full">
                 <header className="flex justify-between text-primary-text">
@@ -36,7 +61,7 @@ function UserSetting() {
                     <XCircle onClick={() => navigate("/")} />
                 </header>
                 <section className="flex items-center mt-20 flex-col">
-                    <div className="setting-form">
+                    <div className="setting-form text-secondary-text">
                         {/* Automatic generated id */}
                         <p className="label-id">User ID</p>
                         <div className="flex gap-2 items-center input-id">
@@ -44,7 +69,7 @@ function UserSetting() {
                                 type="text"
                                 placeholder="Save to generate code"
                                 readOnly
-                                className="p-2 h-10 bg-message-input outline-none rounded-[5px] text-black"
+                                className="p-2 h-10 bg-message-input outline-none rounded-[5px]"
                                 value={userData.id}
                             />
                             <Copy />
@@ -54,36 +79,30 @@ function UserSetting() {
                         <p className="label-name">Display Name</p>
                         <input
                             type="text"
-                            className="p-2 h-10 w-75 bg-message-input outline-none input-name rounded-[5px] text-black"
+                            className="p-2 h-10 w-75 bg-message-input outline-none input-name rounded-[5px]"
                             value={displayName}
                             onChange={(e) => setDisplayName(e.target.value)}
                         />
 
                         {/* Save Button */}
                         <button
-                            className="p-2 bg-accent save-button text-secondary-text font-medium rounded-[5px]"
+                            className="p-2 bg-accent save-button text-black font-medium rounded-[5px]"
                             onClick={saveUser}
                         >
-                            Save Button
+                            Save Data
                         </button>
 
                         {/* More Option */}
 
                         <p className="label-option">More Option</p>
 
-                        <div className="w-full flex flex-col md:flex-row justify-around more-button gap-4">
-                            <button className="p-2 rounded-[5px] flex gap-2 text-white bg-green-500 items-center justify-center">
-                                <Download size={18} />
-                                <p>Download Data</p>
-                            </button>
-                            <button
-                                className="p-2 rounded-[5px] flex gap-2 text-white bg-red-500 items-center justify-center"
-                                onClick={() => setWarn(true)}
-                            >
-                                <Trash size={18} />
-                                <p>Wipe Data</p>
-                            </button>
-                        </div>
+                        <button
+                            className="p-2 rounded-[5px] flex gap-2 text-white bg-red-500 items-center justify-center more-button"
+                            onClick={() => setWarn(true)}
+                        >
+                            <Trash size={18} />
+                            <p>Wipe Data</p>
+                        </button>
                     </div>
                 </section>
             </div>
