1783bd06e481bfc95391418febfa3abc5b64a1e2
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-24 08:51:40 +0700

    feat: add small logic for expanded search bar
    - Expand search bar when focus and shrink button add

--- kanban-app/src/components/sidebar/Sidebar.tsx
+++ kanban-app/src/components/sidebar/Sidebar.tsx
@@ -3,23 +3,50 @@ import {
     FolderClosed,
     LogOut,
     Plus,
+    Search,
     Sun,
     User,
 } from "lucide-react";
+import { useState } from "react";
 
 function Sidebar() {
+    const [searchValue, setSearchValue] = useState<string>("");
+    const [isFocused, setIsFocused] = useState<boolean>(false);
+
+    const isExpanded = isFocused || searchValue.length > 0;
+
     return (
         <main className="w-75 border-r h-screen p-4 flex flex-col gap-4">
-            {/* Button Section */}
+            {/* Search and Create Section */}
             <section className="flex gap-2">
-                <button className="flex-1 flex items-center justify-center gap-2 bg-orange-300 rounded-[5px]">
-                    <Plus />
-                    <p className="font-semibold">Create Project</p>
+                {/* Create new project */}
+                <button
+                    className={`${isExpanded ? "w-10 h-8.75" : "flex-1"} flex items-center justify-center gap-2 bg-orange-300 rounded-[5px]`}
+                >
+                    <Plus className="shrink-0" />
+                    <p
+                        className={`${isExpanded ? "hidden" : "block"} font-semibold`}
+                    >
+                        Create Project
+                    </p>
                 </button>
-                <input
-                    type="text"
-                    className="w-10 h-8.75 bg-orange-300 rounded-[5px]"
-                />
+                {/* Search Project */}\
+                <div
+                    className={`${isExpanded ? "flex-1" : "w-10 h-8.75"} bg-orange-300 rounded-[5px] relative`}
+                >
+                    <input
+                        type="text"
+                        className="w-full h-full outline-none p-2"
+                        value={searchValue}
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setIsFocused(false)}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                    />
+                    <Search
+                        className="absolute right-2.5 top-1/2 -translate-y-1/2 pointer-events-none"
+                        size={20}
+                    />
+                </div>
             </section>
 
             {/* Project section */}
