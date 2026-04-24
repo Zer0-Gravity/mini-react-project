52d0299a6691351f492a64eb5c47287d02582f60
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-24 11:19:18 +0700

    feat: add button tab logic
    - Use object array
    - add animation using framer motion

--- kanban-app/src/components/page/ProjectPage.tsx
+++ kanban-app/src/components/page/ProjectPage.tsx
@@ -1,6 +1,24 @@
-import { Filter, Plus, Search, SortDesc } from "lucide-react";
+import {
+    Filter,
+    List,
+    Plus,
+    Search,
+    SortDesc,
+    SquareKanban,
+    Table2,
+} from "lucide-react";
+import { motion } from "motion/react";
+import { useState } from "react";
+
+const tabs = [
+    { id: "Board", icon: <SquareKanban size={20} /> },
+    { id: "Table", icon: <Table2 size={20} /> },
+    { id: "List", icon: <List size={20} /> },
+];
 
 function ProjectPage() {
+    const [activeTab, setActiveTab] = useState<string>(tabs[0].id);
+
     return (
         <main className="h-screen w-screen">
             <section className="p-6 border-b space-y-6">
@@ -14,7 +32,31 @@ function ProjectPage() {
 
                 <div className="flex items-center justify-between">
                     {/* Tab button */}
-                    <div></div>
+                    <div className="bg-gray-300 rounded-[5px] p-2 flex gap-5">
+                        {tabs.map((tab, index) => (
+                            <button
+                                key={index}
+                                className={`relative w-25 h-10 ${activeTab === tab.id ? "text-white" : "text-black/40"} transition-colors grid place-items-center`}
+                                onClick={() => setActiveTab(tab.id)}
+                            >
+                                {activeTab === tab.id && (
+                                    <motion.div
+                                        className="absolute inset-0 bg-amber-600 z-0 rounded-[5px]"
+                                        layoutId="activePill"
+                                        transition={{
+                                            type: "spring",
+                                            stiffness: 380,
+                                            damping: 30,
+                                        }}
+                                    />
+                                )}
+
+                                <div className="flex items-center gap-2 relative z-10">
+                                    {tab.icon} <span>{tab.id}</span>
+                                </div>
+                            </button>
+                        ))}
+                    </div>
 
                     {/* Other utility button */}
                     <div className="flex items-center gap-4">
