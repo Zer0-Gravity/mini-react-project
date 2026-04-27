bde62104c9e49025fcf0f30735d7da7bb44c5a52
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-27 11:41:37 +0700

    feat: add ui animation on the popover

--- kanban-app/src/components/utils/Popover.tsx
+++ kanban-app/src/components/utils/Popover.tsx
@@ -1,4 +1,5 @@
 import { PenLine, Trash } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
 import type React from "react";
 import type { ElementType } from "react";
 
@@ -9,14 +10,19 @@ interface PopoverType {
 
 function Popover({ floating, floatingStyle }: PopoverType) {
     return (
-        <div
-            className="absolute right-0 p-2 flex flex-col gap-1 bg-gray-100 shadow-lg rounded-lg w-fit"
-            ref={floating}
-            style={floatingStyle}
-        >
-            <ButtonPop desc="Rename" icon={PenLine} color="green" />
-            <ButtonPop desc="Delete" icon={Trash} color="red" />
-        </div>
+        <AnimatePresence>
+            <motion.div
+                className="absolute right-0 p-2 flex flex-col gap-1.5 bg-gray-100 shadow-lg rounded-lg w-40 z-20"
+                ref={floating}
+                style={floatingStyle}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+            >
+                <ButtonPop desc="Rename" icon={PenLine} color="green" />
+                <ButtonPop desc="Delete" icon={Trash} color="red" />
+            </motion.div>
+        </AnimatePresence>
     );
 }
 
@@ -28,7 +34,9 @@ interface ButtonType {
 
 function ButtonPop({ desc, icon: Icon, color }: ButtonType) {
     return (
-        <button className={`flex gap-2 items-center text-${color}-600`}>
+        <button
+            className={`flex gap-2 items-center text-${color}-600 hover:bg-gray-400 p-0.5 rounded-sm`}
+        >
             <Icon size={18} />
             <span>{desc}</span>
         </button>
