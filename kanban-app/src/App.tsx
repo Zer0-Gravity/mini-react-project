210a0cbce323c467c5ff9430e4ff063e8f30511a
Zer0-Gravity <muslikhulimam878@gmail.com>
2026-04-29 09:18:09 +0700

    feat: make skeleton modal jsx for the rename and delete

--- kanban-app/src/App.tsx
+++ kanban-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router";
 import Sidebar from "./components/sidebar/Sidebar";
 import ProjectPage from "./components/page/ProjectPage";
+import GlobalModal from "./components/utils/GlobalModal";
 
 function App() {
     return (
         <BrowserRouter>
-            <main className="flex">
+            <main className="flex relative">
+                <GlobalModal />
                 <Sidebar />
                 <div className="flex-1 min-w-0">
                     <ProjectPage />
